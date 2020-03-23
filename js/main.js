afd850e0d12b656ea6efd9a1e0cf1867f031e2cd
John Montejano <johnmontejano@Johns-MacBook-Pro.local>
2020-03-23 11:58:31 -0700

    get started section

--- js/main.js
+++ js/main.js
@@ -17,4 +17,33 @@ document.addEventListener('DOMContentLoaded', function() {
 document.addEventListener('DOMContentLoaded', function() {
     var timepicker = document.querySelectorAll('.timepicker');
     M.Timepicker.init(timepicker, {});
+    var parallax = document.querySelectorAll('.parallax');
+    M.Parallax.init(parallax, {});
   });
+
+  function initialize() {
+
+    var ac = new google.maps.places.Autocomplete(
+      (document.getElementById('address_field')), {
+        types: ['geocode']
+      });
+  
+    ac.addListener('place_changed', function() {
+  
+      var place = ac.getPlace();
+  
+      if (!place.geometry) {
+        // User entered the name of a Place that was not suggested and
+        // pressed the Enter key, or the Place Details request failed.
+        window.alert("No details available for input: '" + place.name + "'");
+        return;
+      }
+  s
+      var html = '<div>Latitude: ' + place.geometry.location.lat() + '</div>';
+      html += '<div>Longitude: ' + place.geometry.location.lng() + '</div>';
+  
+      document.getElementById('address_field').innerHTML = html;
+    });
+  }
+  
+  initialize();
