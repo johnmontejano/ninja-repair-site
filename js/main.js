4f9d42b38a5d6216a89d55c28ae36374b3af464b
John Montejano <johnmontejano@Johns-MacBook-Pro.local>
2020-03-25 13:42:40 -0700

    [update]replaced placeholder text

--- js/main.js
+++ js/main.js
@@ -1,49 +1,36 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems, {});
-  });
+document.addEventListener("DOMContentLoaded", function() {
+  var elems = document.querySelectorAll(".sidenav");
+  var instances = M.Sidenav.init(elems, {});
+});
 // SELECT DROPDOWN
-  document.addEventListener('DOMContentLoaded', function() {
-    var select = document.querySelectorAll('select');
-    var instances = M.FormSelect.init(select, {});
-  });
+document.addEventListener("DOMContentLoaded", function() {
+  var select = document.querySelectorAll("select");
+  var instances = M.FormSelect.init(select, {});
+});
 // DATEPICKER
-  document.addEventListener('DOMContentLoaded', function() {
-    var datepicker = document.querySelectorAll('.datepicker');
-    var instances = M.Datepicker.init(datepicker, {});
-  });
+document.addEventListener("DOMContentLoaded", function() {
+  var datepicker = document.querySelectorAll(".datepicker");
+  var instances = M.Datepicker.init(datepicker, {});
+});
 // TIMEPICKER
 
-document.addEventListener('DOMContentLoaded', function() {
-    var timepicker = document.querySelectorAll('.timepicker');
-    M.Timepicker.init(timepicker, {});
-    var parallax = document.querySelectorAll('.parallax');
-    M.Parallax.init(parallax, {});
-  });
+document.addEventListener("DOMContentLoaded", function() {
+  var timepicker = document.querySelectorAll(".timepicker");
+  M.Timepicker.init(timepicker, {});
+  var parallax = document.querySelectorAll(".parallax");
+  M.Parallax.init(parallax, {});
+});
 
-  function initialize() {
-
-    var ac = new google.maps.places.Autocomplete(
-      (document.getElementById('address_field')), {
-        types: ['geocode']
-      });
-  
-    ac.addListener('place_changed', function() {
-  
-      var place = ac.getPlace();
-  
-      if (!place.geometry) {
-        // User entered the name of a Place that was not suggested and
-        // pressed the Enter key, or the Place Details request failed.
-        window.alert("No details available for input: '" + place.name + "'");
-        return;
+$(document).ready(function() {
+  $(function() {
+    $(window).scroll(function() {
+      if ($(this).scrollTop() > 380) {
+        $("#nav").addClass("changeColor");
+        console.log("touch");
       }
-  s
-      var html = '<div>Latitude: ' + place.geometry.location.lat() + '</div>';
-      html += '<div>Longitude: ' + place.geometry.location.lng() + '</div>';
-  
-      document.getElementById('address_field').innerHTML = html;
+      if ($(this).scrollTop() < 370) {
+        $("#nav").removeClass("changeColor");
+      }
     });
-  }
-  
-  initialize();
+  });
+});
